9cc66839db5fa4338f04628d12db161378bbed3f
Thomas Naegele <xonic@gmx.eu>
2014-09-09 22:00:49 +0200

    - change navigation animation - add minimal-ui viewport setting

--- themes/gp-dolomiti/header--plain.php
+++ themes/gp-dolomiti/header--plain.php
@@ -33,7 +33,7 @@
 <?php echo '<!-- ' . get_the_ID() . ' -->'; ?>
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
-		<meta name="viewport" content="width=device-width,initial-scale=1.0" />
+		<meta name="viewport" content="width=device-width,initial-scale=1.0,minimal-ui" />
 		<title><?php
 			/*
 			 * Print the <title> tag based on what is being viewed.
