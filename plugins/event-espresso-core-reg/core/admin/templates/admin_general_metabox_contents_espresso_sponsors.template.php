a345a29cc82fec50729b1828cc339504bc57f30c
Thomas Naegele <xonic@gmx.eu>
2014-09-15 23:12:10 +0200

    - add ee4 files to repo

---
+++ plugins/event-espresso-core-reg/core/admin/templates/admin_general_metabox_contents_espresso_sponsors.template.php
@@ -0,0 +1,5 @@
+<div class="padding">
+	<iframe id="ee-ads-iframe" src="http://eventespresso.com/ee4-admin-sidebar.php">
+		<p><?php _e('Your browser doesn\'t support iframes. What a pity.', 'event_espresso'); ?></p>
+	</iframe>
+</div>
